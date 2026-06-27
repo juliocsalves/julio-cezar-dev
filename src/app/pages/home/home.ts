5ae73c5c4514a1a295e44cc15d701cac5027bf28
Julio Cezar <juliocsalves@gmail.com>
2026-06-27 15:50:39 -0300

    Adiciona as seções Stack Tecnológica e Chamada para Ação da página inicial

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero/hero'
+import { Hero } from './hero/hero';
+import { Features } from './features/features';
+import { Projects } from './projects/projects';
+import { Process } from './process/process';
+import { Stack } from './stack/stack';
+import { Cta } from './cta/cta';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero,Features, Projects, Process, Stack, Cta],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
---
+++ src/app/pages/home/stack/stack.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stack',
+  imports: [],
+  templateUrl: './stack.html',
+  styleUrl: './stack.css',
+})
+export class Stack {}
